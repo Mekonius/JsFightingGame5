dc6286825571d3b01f4b4d4432368b5fa6493072
Kenneth Løvgren <kenneth.l.thomsen@gmail.com>
2022-12-14 09:36:25 +0100

    Attackbox Logic

--- index.js
+++ index.js
@@ -8,16 +8,41 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.7;
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({position, velocity, color}) {
         this.position = position
         this.velocity = velocity
         this.height = 150
+        this.width = 50
         this.lastKey
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50
+        }
+        this.color = color
+        this.isAttacking
     }
 
     draw () {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillStyle = this.color
+        c.fillRect(
+            this.position.x, 
+            this.position.y, 
+            this.width, 
+            this.height
+        )
+
+
+        // attack box
+        c.fillStyle = 'purple'
+        c.fillRect(
+            this.attackBox.position.x, 
+            this.attackBox.position.y, 
+            this.attackBox.width, 
+            this.attackBox.height
+        )
+
+
     }
 
     update() {
@@ -41,7 +66,8 @@ const player = new Sprite({
     velocity: {
         x: 0,
         y: 0,
-    }
+    },
+    color: 'blue'
 })
 
 const keys = {
@@ -77,7 +103,8 @@ const enemy = new Sprite({
     velocity: {
         x: 0,
         y: 0
-    }
+    },
+    color: 'red'
 })
 let lastKey
 
@@ -107,6 +134,21 @@ function animate() {
         enemy.velocity.x = -5
     } else if (keys.ArrowRight.isDown && enemy.lastKey === 'ArrowRight') {
         enemy.velocity.x = 5
+    }
+
+    if(
+        player.attackBox.position.x + 
+        player.attackBox.width >= 
+        enemy.position.x && 
+        player.attackBox.position.x <= 
+        enemy.position.x + enemy.width &&
+        player.attackBox.position.y + player.attackBox.height >= 
+        enemy.position.y &&
+        player.attackBox.position.y <= 
+        enemy.position.y + enemy.height &&
+        player.isAttacking
+    )    {
+        console.log('Aouch 💣 !!')
     }
 
 }
